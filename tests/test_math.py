49fdaf2bfacb3c1e85576ce8fe0459b103270942
Angele265 <angelevelda@yahoo.com>
2026-08-06 15:35:12 +0200

    Add test case

--- tests/test_math.py
+++ tests/test_math.py
@@ -6,9 +6,17 @@ def sub(a, b):
     return a - b
 
 
+def mult(a, b):
+    return a * b
+
+
 def test_add():
     assert add(2, 3) == 5
 
 
 def test_sub():
     assert add(10, 3) == 7
+
+
+def test_mult():
+    assert add(10, 3) == 30
